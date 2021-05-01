b9a1299bd775f6aebf392f2fc59c540074280f96
Vladislav Chernyshov <unbeliever1806@gmail.com>
2021-05-01 19:47:42 +0300

    feat: added new elements for review-card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import {ReviewCard} from './components'
-import './App.scss';
+import { ReviewCard } from './components'
+import './styles/main.scss'
 
 function App() {
   return (
-    <div className="App">
+    <div 
+    className="App" 
+    style={{ 
+      background: '#E5E5E5', 
+      width: '100%',
+      padding: '40px',
+      }}
+    >
       <header className="App-header">
       </header>
       <ReviewCard />
--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,61 +1,90 @@
+import {
+  DefaultUserIcon,
+  FlagUAIcon,
+  StarIcon,
+  DotsIcon,
+  LikeIcon,
+  DislikeIcon,
+  PositiveSideIcon,
+  NegativeSideIcon
+} from './icons';
 import '../styles/main.scss';
 
 export const ReviewCard = () => {
   return (
-    <div className="reviews">
+    <div className="reviews" style={{ margin: 'auto' }}>
       <header className="reviews__header">
-        <span>avatar</span>
+        <span className="reviews__header__avatar">
+          <DefaultUserIcon />
+        </span>
+
         <div className="reviews__header__wrapper">
 
           <div className="reviews__header__wrapper__top">
+            <div className="reviews__header__wrapper__top--info">
 
-            <span className="reviews__header__wrapper__top__name">name</span>
-            <span className="reviews__header__wrapper__top__role--green">role</span>
-            <span className="reviews__header__wrapper__top__country">county</span>
-            <span className="reviews__header__wrapper__top__date">23.02.2019 в 15:06</span>
-        
+              <span className="reviews__header__wrapper__top--info--name">Александр</span>
+              <span className="reviews__header__wrapper__top--info--green-role">Опытный</span>
+              <span className="reviews__header__wrapper__top--info--country">
+                <FlagUAIcon />
+                <span className="reviews__header__wrapper__top--info--country--text">
+                  Украина
+                </span>
+              </span>
+              <span className="reviews__header__wrapper__top--info--date">23.02.2019 в 15:06</span>
+            </div>
+
+            <div className="reviews__header__aside">
+              <StarIcon />
+              <span className="reviews__header__aside__rating">4.8</span>
+            </div>
           </div>
           <div className="reviews__header__wrapper__bottom">
-            <span className="reviews__header__wrapper__bottom__messages-amount">235 сообщений</span>
-            <span className="reviews__header__wrapper__bottom__bull">&bull;</span>
-            <span className="reviews__header__wrapper__bottom__adress">
-              Музыкальный магазин “Super Sound”, Москва,... Перейти
-              {/* (if length isn't enough) make it short and put link in it as "Перейти" */}
+            <span className="reviews__header__wrapper__bottom--messages-amount">235 сообщений</span>
+            <span className="reviews__header__wrapper__bottom--bull">&bull;</span>
+            <span className="reviews__header__wrapper__bottom--adress">
+              Музыкальный магазин “Super Sound”, Москва,...
+              <a
+                className="reviews__header__wrapper__bottom--adress--link"
+                href="http://localhost:3000/" 
+                target="_blank" 
+                rel="noreferrer noopener"
+              >Перейти</a>
             </span>
           </div>
         </div>
-        <div className="reviews__header__aside">
-          <span>**</span>
-          <span class="reviews__header__aside__rating">4.8</span>
-        </div>
-        <span>dots</span>
+        <DotsIcon />
       </header>
 
       <div className="reviews__body">
-        <div>
-          <img src="" alt="Good sides"/>
-          <div>
+        <div className="reviews__body__review">
+          <div className="reviews__body__review--icon">
+            <PositiveSideIcon />
+          </div>
+          <div className="reviews__body__review--text">
             Предназначен для опытных инвесторов они смогут совершать сделки на бирже и видеть всю информацию о торгах сразу на нескольких экранах, в любом браузере и на любом количестве устройств. Предназначен для опытных инвесторов.
           </div>
         </div>
-        <div>
-          <img src="" alt="Bad sides"/>
-          <div>
+        <div className="reviews__body__review">
+          <div className="reviews__body__review--icon">
+            <NegativeSideIcon />
+          </div>
+          <div className="reviews__body__review--text">
             Предназначен для опытных инвесторов.
           </div>
         </div>
       </div>
 
       <footer className="reviews__footer">
-        <button type="button">Ответить</button>
+        <button className="reviews__footer__button" type="button">Ответить</button>
         <div className="reviews__footer__wrapper">
           <div className="reviews__footer__wrapper__interaction-button">
-            <img src="" alt="Like button"/>
-            <div>Like</div>
+            <LikeIcon alt="asd" />
+            <span className="reviews__footer__wrapper__interaction-button--number">23</span>
           </div>
           <div className="reviews__footer__wrapper__interaction-button">
-            <img src="" alt="Dislike button"/>
-            <div>Dislike</div>
+            <DislikeIcon />
+            <span className="reviews__footer__wrapper__interaction-button--number">10</span>
           </div>
         </div>
       </footer>
